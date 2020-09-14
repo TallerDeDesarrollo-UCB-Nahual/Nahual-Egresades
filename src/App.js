3333f017d1af559e2fe04c63ed975815c6a3df0e
laura Meneses <lmeneses58@gmail.com>
2020-09-14 11:44:37 -0400

    table

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './lista-egresades/Navbar';
+import Nahual_Table from './lista-egresades/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         NAHUAL
-        </p>
-        
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Navbar} />
+        <Route exact path="/table" component={Nahual_Table} />
+      </Switch>
+    </Router>
   );
 }
 
