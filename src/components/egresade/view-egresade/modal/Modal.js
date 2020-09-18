d1aa394756d04f567c963c3626bc0f4e578b5310
Luis Toro <luis.toro.flores97@gmail.com>
2020-09-17 22:17:41 -0400

    map data from an egresade

--- src/components/egresade/view-egresade/modal/Modal.js
+++ src/components/egresade/view-egresade/modal/Modal.js
@@ -1,31 +1,47 @@
 import React from 'react'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import see from '../../../../images/see.png';
+import LogoNahual from '../../../../images/proyecto-nahual.webp'
 
 function ModalExampleModal(props) {
   const [open, setOpen] = React.useState(false)
-
+  console.log(props);
   return (
     <Modal
-      onClose={props.closeModal}
+      open={open}
+      onClose={() => setOpen(false)}
       onOpen={() => setOpen(true)}
-      open={props.open}
-      trigger={<Button>Show Modal</Button>}
+      size="small"
+      closeIcon
+      trigger={<Button className="button"> 
+        <img src={see} className="icon"></img>
+        <label className="icon-text">Ver</label>
+        </Button>}
     >
-      <Modal.Header>Select a Photo</Modal.Header>
+
+
+      <Modal.Header >
+        <span><Image src={LogoNahual} size='small' />
+        <h1>{props.name}</h1>
+        </span>
+
+      </Modal.Header>
       <Modal.Content image>
         <Image size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' wrapped />
         <Modal.Description>
           <Header>Default Profile Image</Header>
           <p>
             We've found the following gravatar image associated with your e-mail
-            address.
+            address. My age is {props.age}
           </p>
-          <p>Is it okay to use this photo?</p>
+          <p>Is it okay to use this photo? {props.message}</p>
+
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
         <Button color='black' onClick={() => setOpen(false)}>
           Nope
+
         </Button>
         <Button
           content="Yep, that's me"
--- src/lista-egresades/Table.js
+++ src/lista-egresades/Table.js
@@ -1,154 +1,155 @@
 import React, { Component } from 'react'
 import { Icon, Label, Menu, Table } from 'semantic-ui-react';
-import Modal from '../utils/modal/Modal';
+import Modal from '../components/egresade/view-egresade/modal/Modal';
 import Navbar from './Navbar';
 import './Table.css';
 import edit from '../images/edit.png';
-import see from '../images/see.png';
 import filter from '../images/filter.png';
 import imp from '../images/imp.png';
 import search from '../images/search.png'
 
-export default class Nahual_Table extends Component{
+export default class Nahual_Table extends Component {
 
-constructor(){
-  super();
-  this.state = {
-    openModal: false,
-    egresades: [
-      {
-        "fullName": "clodomiro saavedra perez",
-        "statusName": "Egresade",
-        "birthDate": "1995-02-01",
-        "mail": "clodo.saavedra@gmail.com",
-        "cellphone": 79840584,
-        "nodeName": "CABA",
-        "graduationYear": 2020,
-        "quarter": 1,
-        "englishLevel": "Basico",
-        "firstJobName": "Tsoft",
-        "linkedin": "https://linkedin/clodomiro",
-        "isEmployed": true,
-        "module": "Testing funcional"
-      },
-      {
-        "fullName": "Test 2",
-        "statusName": "Egresade",
-        "birthDate": "1995-02-01",
-        "mail": "clodo.saavedra@gmail.com",
-        "cellphone": 79840584,
-        "nodeName": "CABA",
-        "graduationYear": 2020,
-        "quarter": 1,
-        "englishLevel": "Intermedio",
-        "firstJobName": "Tsoft",
-        "linkedin": "https://linkedin/clodomiro",
-        "isEmployed": true,
-        "module": "Testing funcional"
-      },
-      {
-        "fullName": "Test 3",
-        "statusName": "Egresade",
-        "birthDate": "1995-06-01",
-        "mail": "clodo.saavedra@gmail.com",
-        "cellphone": 79840584,
-        "nodeName": "CABA",
-        "graduationYear": 2020,
-        "quarter": 1,
-        "englishLevel": "Intermedio",
-        "firstJobName": "Tsoft",
-        "linkedin": "https://linkedin/clodomiro",
-        "isEmployed": true,
-        "module": "Testing Automation"
-      }
-    ]
-  }  
- 
-}
-openModal(){
-  this.setState({openModal: true});
-}
-closeModal(){
-  this.setState({openModal: false});
-}
-render() {
-  return (
-  <div>
-    <Navbar></Navbar>
-    <div className="table">
-    <p className="title">Lista de Egresades</p>
-    <div className="line"></div>
+  constructor() {
+    super();
+    this.state = {
+      openModal: false,
+      egresades: [
+        {
+          "fullName": "clodomiro saavedra perez",
+          "statusName": "Egresade",
+          "birthDate": "1995-02-01",
+          "mail": "clodo.saavedra@gmail.com",
+          "cellphone": 79840584,
+          "nodeName": "CABA",
+          "graduationYear": 2020,
+          "quarter": 1,
+          "englishLevel": "Basico",
+          "firstJobName": "Tsoft",
+          "linkedin": "https://linkedin/clodomiro",
+          "isEmployed": true,
+          "module": "Testing funcional"
+        },
+        {
+          "fullName": "Test 2",
+          "statusName": "Egresade",
+          "birthDate": "1995-02-01",
+          "mail": "clodo.saavedra@gmail.com",
+          "cellphone": 79840584,
+          "nodeName": "CABA",
+          "graduationYear": 2020,
+          "quarter": 1,
+          "englishLevel": "Intermedio",
+          "firstJobName": "Tsoft",
+          "linkedin": "https://linkedin/clodomiro",
+          "isEmployed": true,
+          "module": "Testing funcional"
+        },
+        {
+          "fullName": "Test 3",
+          "statusName": "Egresade",
+          "birthDate": "1995-06-01",
+          "mail": "clodo.saavedra@gmail.com",
+          "cellphone": 79840584,
+          "nodeName": "CABA",
+          "graduationYear": 2020,
+          "quarter": 1,
+          "englishLevel": "Intermedio",
+          "firstJobName": "Tsoft",
+          "linkedin": "https://linkedin/clodomiro",
+          "isEmployed": true,
+          "module": "Testing Automation"
+        }
+      ]
+    }
+  }
 
-      <div className="table-menu">
-        <div className="filter">
-          <img src={filter}></img>
-          <label className="filter1"> Filtrar</label>
+  openModal(event) {
+    this.setState({ openModal: true });
+    console.log(event.target.value);
+  }
+  closeModal() {
+    this.setState({ openModal: false });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar></Navbar>
+        <div className="table">
+          <p className="title">Lista de Egresades</p>
+          <div className="line"></div>
+
+          <div className="table-menu">
+            <div className="filter">
+              <img src={filter}></img>
+              <label className="filter1"> Filtrar</label>
+            </div>
+            <div className="search">
+              <img src={search} className="search-icon"></img>
+              <input className="search-input"></input>
+            </div>
+            <div className="import">
+              <img src={imp} className="menu-icon"></img>
+              <label className="import2"> Importar</label>
+            </div>
+          </div>
+          <br /><br />
+          <Table celled className="table-card">
+            <Table.Header>
+              <Table.Row >
+                <Table.HeaderCell className="table-header">Nombre y Apellido</Table.HeaderCell>
+                <Table.HeaderCell className="table-header">Sede</Table.HeaderCell>
+                <Table.HeaderCell className="table-header">Modulo Cursado</Table.HeaderCell>
+                <Table.HeaderCell className="table-header">Acción</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {this.state.egresades.map((value) => (
+                <Table.Row>
+                  <Table.Cell className="table-border">
+                    <Label className="name" >{value.fullName}</Label><br></br>
+                    <Label className="mail">{value.mail}</Label>
+                  </Table.Cell >
+                  <Table.Cell className="table-border">
+                    <Label className="card-blue">• {value.nodeName}</Label>
+                  </Table.Cell>
+                  <Table.Cell className="table-border">
+                    <Label className="card-green">• {value.module}</Label></Table.Cell>
+                  <Table.Cell colSpan="3" className="table-border">
+                    <img src={edit} className="icon"></img>
+                    <label className="icon-text">Editar</label>
+
+
+                    <Modal value={this} name={value.fullName} open={this.state.openModal}
+                      closeModal={this.closeModal.bind(this)} />
+
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+
+
+            <Table.Footer>
+              <Table.Row>
+                <Table.HeaderCell colSpan='4' className="no-border">
+                  <Menu floated='right' pagination>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron left' />
+                    </Menu.Item>
+                    <Menu.Item as='a'>1</Menu.Item>
+                    <Menu.Item as='a'>2</Menu.Item>
+                    <Menu.Item as='a'>3</Menu.Item>
+                    <Menu.Item as='a'>4</Menu.Item>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron right' />
+                    </Menu.Item>
+                  </Menu>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
         </div>
-        <div className = "search">
-          <img src= {search} className="search-icon"></img>
-          <input className="search-input"></input>
-        </div>
-        <div className= "import">
-          <img src={imp} className="menu-icon"></img>
-          <label className="import2"> Importar</label>
-        </div>
-      </div>
-      <br/><br/>
-      <Table celled className="table-card">
-        <Table.Header>
-          <Table.Row >
-            <Table.HeaderCell className="table-header">Nombre y Apellido</Table.HeaderCell>
-            <Table.HeaderCell className="table-header">Sede</Table.HeaderCell>
-            <Table.HeaderCell className="table-header">Modulo Cursado</Table.HeaderCell>
-            <Table.HeaderCell className="table-header">Acción</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-        {this.state.egresades.map((value) => (
-          <Table.Row>
-            <Table.Cell className="table-border">
-              <Label className="name">{value.fullName}</Label><br></br>
-              <Label className="mail">{value.mail}</Label>
-            </Table.Cell >
-            <Table.Cell className="table-border">
-        <Label className="card-blue">• {value.nodeName}</Label>
-            </Table.Cell>
-            <Table.Cell className="table-border">
-        <Label className="card-green">• {value.module}</Label></Table.Cell>
-            <Table.Cell colSpan="3" className="table-border">
-              <img src={edit} className="icon"></img>
-              <label className="icon-text">Editar</label>
-              <img src={see} className="icon"></img>
-              <label onClick={this.openModal.bind(this)} className="icon-text">Ver</label>
-            </Table.Cell>
-          </Table.Row>
-        ))}
-        </Table.Body>
-        <Modal open={this.state.openModal}
-        closeModal={this.closeModal.bind(this)}/>
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan='4' className="no-border">
-              <Menu floated='right' pagination>
-                <Menu.Item as='a' icon>
-                  <Icon name='chevron left' />
-                </Menu.Item>
-                <Menu.Item as='a'>1</Menu.Item>
-                <Menu.Item as='a'>2</Menu.Item>
-                <Menu.Item as='a'>3</Menu.Item>
-                <Menu.Item as='a'>4</Menu.Item>
-                <Menu.Item as='a' icon>
-                  <Icon name='chevron right' />
-                </Menu.Item>
-              </Menu>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
-      </Table>
-    </div>
-  </div>)
-
-}
-
+      </div>)
+  }
 }
