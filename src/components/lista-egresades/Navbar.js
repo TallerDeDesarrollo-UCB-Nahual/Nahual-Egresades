8cde347fbe411b4ae990ae95ccdac217dedc5a8b
ElYoquer <guillerbauti@gmail.com>
2020-10-12 22:56:48 -0400

    finish

--- src/components/lista-egresades/Navbar.js
+++ src/components/lista-egresades/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../public/images/logo.png';
 import '../../public/stylesheets/Navbar.css';
+import BarraNavegacionAutenticacion from "./../inicio-de-sesion/BarraNavegacionAutenticacion";
 
 export default class Navbar extends Component {
 
@@ -9,6 +10,7 @@ export default class Navbar extends Component {
         <div className="menu">
             <img src={logo} className="logo"></img>
             <label className="nav-titulo">Gestor de Egresades</label>
+            <div className="authNav"><BarraNavegacionAutenticacion /></div>
         </div>
         )
     }
