08d4aa352ac29d31122baa93a7a8dc44fd27727b
Luis Toro <luis.toro.flores97@gmail.com>
2020-10-21 17:19:48 -0400

    add button into navigation bar

--- src/components/inicio-de-sesion/BarraNavegacionAutenticacion.js
+++ src/components/inicio-de-sesion/BarraNavegacionAutenticacion.js
@@ -1,8 +1,10 @@
 import React from "react";
 import BotonAutenticacion from "./BotonAutenticacion";
+import BotonSolicitarAcceso from "./BotonSolicitarAcceso";
 
 const BarraNavegacionAutenticacion = () => (
   <div className="navbar-nav ml-auto">
+    <BotonSolicitarAcceso />
     <BotonAutenticacion />
   </div>
 );
---
+++ src/components/inicio-de-sesion/BotonSolicitarAcceso.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button} from 'semantic-ui-react'
+
+const BotonSolicitarAcceso = () => <Button> Solicitar Acceso </Button>
+
+export default BotonSolicitarAcceso;
