ef8f8eff6e8963cd6925073886e10c413eeeb16a
Luis Toro <luis.toro.flores97@gmail.com>
2020-10-21 20:06:39 -0400

    redirect to microservices

--- src/components/inicio-de-sesion/BotonSolicitarAcceso.js
+++ src/components/inicio-de-sesion/BotonSolicitarAcceso.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button} from 'semantic-ui-react'
 
-const BotonSolicitarAcceso = () => <Button> Solicitar Acceso </Button>
+const BotonSolicitarAcceso = () => <Button  onClick={() => window.location = "http://localhost:3001/"}> Solicitar Acceso </Button>
 
 export default BotonSolicitarAcceso;
