05c2b91bf01f466549a9a9d7e0cd316685350a5e
Andre Pinto <Andreumark@hotmail.com>
2021-03-27 13:21:07 -0400

    NA-EG-FT-I2-9 logica anadida para generar pdf

--- src/components/certificacion/GenerarCertificado.js
+++ src/components/certificacion/GenerarCertificado.js
@@ -1,8 +1,10 @@
 
 import React, { Component, } from 'react';
 import Pdf from "react-to-pdf";
+import { Button } from 'semantic-ui-react'
 import diploma from "../../public/imagenes/diploma.jpg"
 import axios from 'axios';
+import {useHistory} from 'react-router-dom'
 import './CertificadoEstilos.css';
 
 const ref = React.createRef();
@@ -14,39 +16,49 @@ class GenerarCertificado extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        nombre: '',
-        apellido: '',
-        nodo: ''
+      nombre: '',
+      apellido: '',
+      nodo: ''
     };
   }
 
-  obtenerEgresade = async ()=>{
+  obtenerEgresade = async () => {
     const API_URL = `${process.env.REACT_APP_EGRESADES_NAHUAL_API}/egresades/`;
     let egresadeDato;
-    try{
-      egresadeDato=await axios.get(`${API_URL}${this.props.match.params.id}${"/DTO"}`);
-      this.setState({nombre:egresadeDato.data.response.nombre});
-      this.setState({apellido:egresadeDato.data.response.apellido});
-      this.setState({nodo:egresadeDato.data.response.nodo});
-    }catch(error)
-    {}
+    try {
+      egresadeDato = await axios.get(`${API_URL}${this.props.match.params.id}${"/DTO"}`);
+      this.setState({ nombre: egresadeDato.data.response.nombre });
+      this.setState({ apellido: egresadeDato.data.response.apellido });
+      this.setState({ nodo: egresadeDato.data.response.nodo });
+    } catch (error) { }
   }
-componentDidMount(){
-  this.obtenerEgresade();
-}
+  componentDidMount() {
+    this.obtenerEgresade();
+  }
+  retornar(){
+    this.props.history.push("/listaEgresades");
+  }
   render() {
-    const {nombre,apellido,nodo}=this.state;
+    const { nombre, apellido, nodo } = this.state;
     return (
       <>
-        <div className="Post" ref={ref}>
-          <div className="Datos">
+        <div className="certificado" ref={ref}>
+          <div className="datos">
             <h2 className="nodoEgresade">{nodo}</h2>
             <h1 className="nombreEgresade">{nombre} {apellido}</h1>
           </div>
         </div>
-        <Pdf options={options} targetRef={ref} filename="certificado.pdf">
-          {({ toPdf }) => <button onClick={toPdf}>Capture as PDF</button>}
-        </Pdf>
+        <div className="pdfBotones">
+          <Button color='red' onClick={()=>{this.retornar()}} >
+            <i className="cancel icon"></i>
+            <label className="icon-text">Cancelar</label>
+          </Button>
+          <Pdf x={-5} y={-0.3} scale={0.95} options={options} targetRef={ref} filename={this.state.nombre + this.state.apellido + '.pdf'}>
+            {({ toPdf }) => <Button onClick={toPdf} color='green'>Descargar PDF</Button>}
+          </Pdf>
+
+        </div>
+
       </>
     );
   }
